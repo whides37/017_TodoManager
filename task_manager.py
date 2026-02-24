8805218a4a71ff6a55fcb6d40a36c7a4a0d5a0d5
whides37 <w.hides37@gmail.com>
2026-02-24 10:09:47 +0900

    タスク追加削除メソッド、完了処理メソッド追記

---
+++ task_manager.py
@@ -0,0 +1,34 @@
+from task import Task
+import json
+
+class TaskManager:
+
+    def __init__(self):
+        self.tasks = []
+        self.load_data()
+
+        #タスク追加
+        def add_task(self,title,deadline_str):
+            task = Task(title,deadline_str)
+            self.tasaks.append(task)
+            self.save.data()
+
+        #タスク削除
+        def remove_task(self,title):
+            for task in self.tasks:
+                if task.title ==title:
+                    self.tasks.remove(task)
+                    self.save.data()
+                    return
+            raise KeyError("そのタスクは存在しません")
+        
+        #完了処理
+        def complete_task(self,title):
+            for task in self.tasks:
+                if task.title == title:
+                    task.mark_completed()
+                    self.save_data()
+                    return
+            raise KeyError("そのタスクは存在しません")
+        
+        #一覧表示
