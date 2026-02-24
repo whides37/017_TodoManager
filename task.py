f6e3527a86e3172292d80f1c322b883a7ae07b00
whides37 <w.hides37@gmail.com>
2026-02-24 09:50:52 +0900

    JSON読み込みメソッド修正

--- task.py
+++ task.py
@@ -1,14 +1,26 @@
+from detetime import datetime
 import json
 
 class Task:
 
     #JSONを読み込む
-    def __init__(self,json_path ="students_data.json"):
-        self.json_path = json_path
+    def __init__(self,title,deadline_str):
+        self.title = title
+
+        #文字列をdatetimeに変換
         try:
-            with open(json_path,"r",encoding="utf=8")as f:
-                self.students = json.load(f)
-        except(FileNotFoundError,json.JSONDecodeError):
-            self.students ={}
+            self.deadline = datetime.strptime(deadline_str,"%Y-%m-%d")
+        except ValueError:
+            raise ValueError("日付はYYYY-MM-DD形式で入力してください")
+        self.completed = False
 
     #
+    def mark_completed(self):
+        self.completed = True
+
+    def is_overdue(self):
+        return datetime.now() > self.deadline and not self.completed
+    
+    def __str__(self):
+        status = "完了" if self.completed else "未完了"
+        return f"{self.title}| 期限:{self.deadline.date()} | 状態:{status}" 
